39b9c386b2252e215bea7db0e4bc6b1cc33ae26f
student <annasukhoplesko@gmail.com>
2023-05-02 18:54:41 +0300

    time sleepy

--- fgfg.py
+++ fgfg.py
@@ -8,3 +8,16 @@ def my_decorator_func(func): #3
 def say_hello(): #1
     print('Hello!')
 say_hello()
+
+#2
+import time
+def delay_decorator(func):
+    def wrapper(*args,**krargs):
+        time.sleep(3)
+        return func(*args,**krargs)
+    return wrapper
+
+@delay_decorator
+def sleepy():
+    print('I am sleping..')
+sleepy()
