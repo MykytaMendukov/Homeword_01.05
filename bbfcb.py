0f6a964b35572f86de9d773c4c2724e6e2cf0636
student <annasukhoplesko@gmail.com>
2023-05-04 20:53:02 +0300

    chinpin'

--- bbfcb.py
+++ bbfcb.py
@@ -1,12 +1,42 @@
-def a(func):
-    def wrapper(*args, **kwargs):
-        k = func(*args, **kwargs) + 100
-        print(k)
-    return wrapper
-@a
-def f(*args):
-    r = 1
-    for i in args:
-        r = r * i
-    return r
-f(1,2,3,4)
+#1
+# class Student():
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+#     def info(self):
+#         print(f"Ім'я: {self.name}, Вік: {self.age}")
+# student = Student('Oleg', 15)
+# student.info()
+
+#2
+
+# class Circle:
+#     def __init__(self, radius):
+#         self.radius = radius
+#     def area(self):
+#         print('Area: ',self.radius**2 * 3.14)
+# circle = Circle(float(input('Enter radius: ')))
+# circle.area()
+
+
+#3
+# class Animal:
+#     def __init__(self, sound):
+#         self.sound = sound
+# Dog = Animal('Гав!')
+# print(Dog.sound)
+
+#4
+
+class BankAccount:
+    def __init__(self, balance, owner):
+        self.balance = balance
+        self.owner = owner
+    def deposit(self):
+        print(self.balance + b)
+    def withdraw(self):
+        print(self.balance - b)
+bank = BankAccount(10000, 'Oleg')
+bank.()
+bank.deposit()
+
